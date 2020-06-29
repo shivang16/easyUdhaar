d5bda2e331b60b5a4d895442a9baac660b2b4f25
dB2510 <dhruvbodani2510@gmail.com>
2020-06-30 02:14:38 +0530

    dotenv error rectified

--- server/app.js
+++ server/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
 //Connect to DB
 dotenv.config();
 
@@ -12,7 +20,7 @@ console.log("DB CONNECTED!!");
 
 //Import Routes
 const authRoute = require('./routes/auth.js');
-const privateRoute = require('./routes/private_route');
+// const privateRoute = require('./routes/private_route');
 const dashboard = require('./routes/dashboard');
 const newCampaign = require('./routes/newCampaign');
 const verification = require('./routes/verification');
@@ -21,7 +29,28 @@ const transaction = require('./routes/transactionHistory');
 const payment = require('./routes/payment');
 const repayment = require('./routes/repayment');
 
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
+import Template from './../template';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import Routes from './../client/Routes';
+import { StaticRouter } from 'react-router-dom';
+import theme from './../client/theme';
+import devBundle from './devBundle';
+
+devBundle.compile(app);
+
 app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 // Adding Middleware
 app.use('/user',authRoute);
@@ -31,7 +60,7 @@ app.use('/verification',verification);
 app.use('/newLending',newLender);
 app.use('/transaction',transaction);
 app.use('/payment',payment);
-app.use('/pr',privateRoute);
+// app.use('/pr',privateRoute);
 app.use('/repayment',repayment);
 
 
@@ -39,9 +68,36 @@ app.get('/',function(req,res){
     res.send("Yoo");
 });
 
-app.get('*',(req,res)=>{
-    res.send("PAGE NOT FOUND!!");
-});
+app.get('*', (req, res) => {
+    const sheets = new ServerStyleSheets()
+    const context = {}
+    const markup = ReactDOMServer.renderToString(
+      sheets.collect(
+            <StaticRouter location={req.url} context={context}>
+              <ThemeProvider theme={theme}>
+                <Routes />
+              </ThemeProvider>
+            </StaticRouter>
+          )
+      )
+      if (context.url) {
+        return res.redirect(303, context.url)
+      }
+      const css = sheets.toString()
+      res.status(200).send(Template({
+        markup: markup,
+        css: css
+      }))
+  })
+
+  app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message})
+    }else if (err) {
+      res.status(400).json({"error" : err.name + ": " + err.message})
+      console.log(err)
+    }
+  })
     
 app.listen(3000,function(req,res){
     console.log("SERVER IS ON!!");
