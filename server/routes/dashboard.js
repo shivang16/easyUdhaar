4edddee575d5843e8633fcf021b1ae7af35b7c82
shivang16 <shivangmangal@gmail.com>
2020-07-01 00:50:30 +0530

    backend dashboard updated

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -13,25 +13,46 @@ router.get('/',verify,async (req,res)=>{
 
     if(user.accountType == false)
     {
+        
         // Lender
         var totalAmountLend = 0;
         var totalAmountRecived = 0;
         var totalLending = 0;
-    await Lender.find({lenderId:user._id},function (err,data) {
+    await Lender.find({lenderId:user._id},async function (err,data) {
         if(err) return res.send(err);
-        
+        var lendingHistory = {};
         for(var i=0;i<data.length;i++)
         {
             totalLending++;
             totalAmountLend+=data[i].amountGiven;
-            totalAmountRecived+=(data[i].amountGiven-data[i].amountToBeRecieved)
+            totalAmountRecived+=(data[i].amountGiven-data[i].amountToBeRecieved);
+            const transactionId = "LID"+String(Math.floor(1000+Math.random()*(8999)));
+            const currentCampaign = await Campaign.findOne({_id:data[i].campaignId});
+            const campaignOwner = await User.findOne({_id:currentCampaign.borrowerId});
+            if(campaignOwner!=null)
+            {
+                var temp = {
+                    "transactionId":transactionId,
+                    "lendingDate":data[i].dateLending,
+                    "status":data[i].repaymentDone,
+                    "name":campaignOwner.firstName
+                };
+                lendingHistory[i] = temp;
+            
+            }
+            
         }
-        const returnObject = {
+        var returnObject = {
             "totalAmountLend":totalAmountLend,
             "totalAmountRecived":totalAmountRecived,
-            "totalLending":totalLending
+            "totalLending":totalLending,
+            "returnInvestment":700
         };
-        return res.send(returnObject);
+        
+        returnObject["data"] = lendingHistory;
+
+
+        return res.json(returnObject);
         //finalResponse['lending'] = returnObject;
     });
 
@@ -45,12 +66,26 @@ router.get('/',verify,async (req,res)=>{
         var amountRepayed = 0;
     await Campaign.find({borrowerId:user._id},async function (err,data) {
         if(err) return res.send(err);
-        console.log("Hoo");
+        var activeCampaign = {};
         for(var i=0;i<data.length;i++)
         {
+            
             totalAmountExpected += (data[i].amount);
             totalAmountGet += (data[i].amountGet);
             amountRepayed += (data[i].amountPaid);
+            if(data[i].running==true)
+            {
+                const cid = "CID"+String(Math.floor(1000+Math.random()*(8999)));
+                const date = String(Math.floor(1+Math.random()*(27)))+'-'+String(Math.floor(1+Math.random()*(6)))+'-2020';
+                var temp = {
+                    "campaignId": cid,
+                    "dueDate": date,
+                    "amountNeeded":data[i].amountExpected
+                     // cam id duedate amount req
+                }
+                activeCampaign[i] = temp;
+            }
+            
         }
         campaignProgress = totalAmountGet/totalAmountExpected;
         repaymentProgress = amountRepayed/totalAmountExpected;
@@ -60,25 +95,13 @@ router.get('/',verify,async (req,res)=>{
             "amountRepayed":amountRepayed,
             "campaignProgress":campaignProgress
         }
-        return res.send(returnObject);
+        returnObject["activeCampaign"] = activeCampaign;
+        return res.json(returnObject);
         //finalResponse['campaign'] = returnObject;
     });
     
     }
 
-    //var finalResponse = {};
-    // Total amount lent
-    
-
-
-    
-    
-   // res.send(finalResponse);
-
-
-
 });
 
 module.exports = router;
-
-
