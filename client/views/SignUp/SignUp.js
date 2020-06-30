db770d45d022e3162df9968fe938e1c18dcc333f
dB2510 <dhruvbodani2510@gmail.com>
2020-07-01 02:44:47 +0530

    signin redirect added in signup

--- client/views/SignUp/SignUp.js
+++ client/views/SignUp/SignUp.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
